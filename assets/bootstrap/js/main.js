6dad9ebfed3a6780be32f29d970067f059ecc735
TrueFlasher <TrueFlasher@mail.ru>
2023-03-23 22:43:07 +0300

    first commit

---
+++ assets/bootstrap/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    const slider = $("#slider").owlCarousel({
+        items:3,
+        // loop:true,
+        margin:50,
+        nav:true,
+        autoplay:true,
+    });
+});
