3fb8d589f097816fbfc804a2d0e32658e517bdf2
TrueFlasher <TrueFlasher@mail.ru>
2023-03-23 23:03:06 +0300

    тест loop

--- assets/bootstrap/js/main.js
+++ assets/bootstrap/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     const slider = $("#slider").owlCarousel({
         items:3,
-        // loop:true,
+        loop:true,
         margin:50,
         nav:true,
         autoplay:true,
