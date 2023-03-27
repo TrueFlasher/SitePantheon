fa1263053f52dbe1692976de09cd8c7acaedb251
TrueFlasher <TrueFlasher@mail.ru>
2023-03-27 22:45:17 +0300

    Редактирование сайта и добавление услуг

--- assets/bootstrap/js/main.js
+++ assets/bootstrap/js/main.js
@@ -1,9 +1,27 @@
 $(document).ready(function(){
     const slider = $("#slider").owlCarousel({
-        items:3,
+
         loop:true,
         margin:50,
-        nav:true,
+
         autoplay:true,
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+
+            700:{
+                items:2
+            },
+
+
+
+            1000:{
+                items:3
+            },
+
+        }
     });
 });
